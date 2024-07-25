47179e498bd30999f2f0b337c51cc496cf87566d
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2024-07-25 13:33:34 -0500

    Create LemonadeStand.java
    Copy and paste this code into a new file and complete it

---
+++ LemonadeStand.java
@@ -0,0 +1,63 @@
+//This program runs a register for a digital lemonade stand
+
+import java.util.Scanner
+
+public class LemonadeStand{
+
+  public static void main(String[] args){
+
+  //------------
+  //  Variables
+  //------------
+  //Create your Scanner object like in past programs
+
+  //create two double type variables
+  //One to store the number of lemonades the user wants to buy
+  //One to store the number of cookies the user wants to buy
+
+    
+  //create a variable called totalCost that will eventually store the customer's final cost
+    
+    
+  //---------
+  //  Prices
+  //---------
+
+    //Set two prices. one for lemonade and one for cookies.
+    // lemonades should cost $0.50 per cup
+    // cookies should cost $1.00 per cookie
+    //hint: what ~type~ of data should these variables be?
+
+
+  //---------
+  // Control 
+  //---------
+
+    System.out.println("******************************************");
+    System.out.println("  WELCOME TO THE DIGITAL LEMONADE STAND!!");
+    System.out.println("******************************************");
+    
+  //Ask the user how many lemonades they want, then ask how many cookies they want. 
+  //Store those two values to the appropriate variables
+
+  //-------
+  // Math
+  //-------
+
+    //Calculate the total cost of the user's purchase and store it to the totalCost
+
+
+  //---------
+  // Result
+  //---------
+
+    //print the total cost of the user's purchase
+
+    System.out.println("******************************");
+    System.out.println();
+    System.out.println("******************************");
+
+    
+  }
+  
+}
