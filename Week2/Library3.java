2bfb7e5a9f8820984e8b5d211e22434571fd22b5
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2024-07-29 12:17:24 -0500

    Create Library3.java

---
+++ Week2/Library3.java
@@ -0,0 +1,31 @@
+//Here is text you'll find useful 
+
+//sorcerersStone, chamberOfSecrets, prisonerOfAzkaban, gobletOfFire, orderOfThePhoenix, halfbloodPrince, deathlyHallows
+
+//"Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire",
+//"Harry Potter and the Order of the Phoenix", "Harry Potter and the Halfblood Prince", "Harry Potter and the Deathly Hallows"
+
+import java.util.Scanner;
+
+public class Library3{
+
+  public static void main(String[] args){
+    
+    Scanner stdIn = new Scanner(System.in);
+
+    String input;
+
+    //TODO use an array of Strings and an array of booleans to simplify the previous library code
+    
+    
+  }
+
+//Method that prints the input string in a pretty way
+  public void prettyPrintHeader(String title){
+    System.out.println("\n");
+    System.out.println("*************************************************");
+    System.out.println("  " + title + "  ");
+    System.out.println("*************************************************");
+    System.out.println("\n");
+  }
+}
