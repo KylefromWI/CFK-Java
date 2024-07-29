ffc6f8232796b85fe3c5ceff698b3b7030483df1
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2024-07-29 14:55:20 -0500

    Simplify to a working file

--- Week2/Library3.java
+++ Week2/Library3.java
@@ -14,9 +14,7 @@ public class Library3{
     Scanner stdIn = new Scanner(System.in);
 
     String input; //stores the choices the user makes
-    
-    //TODO create an array of strings for the titles of the books
-    //TODO create an array of booleans to keep track of which books are checked out
+
     public String[] bookTitles = {"Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire",
 "Harry Potter and the Order of the Phoenix", "Harry Potter and the Halfblood Prince", "Harry Potter and the Deathly Hallows"};
 
@@ -35,44 +33,10 @@ public class Library3{
 
     System.out.print("and that's it!");
     System.out.println("\n \n");
-    
-    
-    //prettyPrintHeader("WELCOME TO THE DIGITAL HARRY POTTER LIBRARY");
-
-    //TODO body of the library will go here
 
     
     stdIn.close();
   } //end main method
-  
-//Method that prints the input string in a pretty way
-/*  public void prettyPrintHeader(String title)
-  {
-    System.out.println("\n");
-    System.out.println("*************************************************");
-    System.out.println("  " + title + "  ");
-    System.out.println("*************************************************");
-    System.out.println("\n");
-  } //end prettyPrintHeader
-*/
-  
-//listCheckedOut: lists the books the user has checked out
-  public void listCheckedOut(String[] checkedOutTitles, boolean[] areCheckedOut)
-  {
-
-    System.out.println("\n");
-    System.out.println("The books you have checked out are: ");
-    
-    for(int i = 0; i < checkedOutBooks.length; i++){
-      if(areCheckedOut[i]){
-        System.out.print(checkedOutTitles[i] + ", ");
-      }
-    } //end for 
-
-    System.out.print("and that's it!");
-    System.out.println("\n \n");
-    
-  } //end listCheckedOut
 
   
 } //end Library3
